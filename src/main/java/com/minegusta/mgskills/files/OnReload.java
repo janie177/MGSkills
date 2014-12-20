78ec4cd5964fc17897dd6f647987bea0722eb8e2
Jan <janie177@hotmail.com>
2014-12-20 17:27:04 +0100

    Removed old conversion.

--- src/main/java/com/minegusta/mgskills/files/OnReload.java
+++ src/main/java/com/minegusta/mgskills/files/OnReload.java
@@ -1,27 +1,13 @@
 package com.minegusta.mgskills.files;
 
-import com.minegusta.mgskills.Main;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
-import java.io.File;
-import java.util.UUID;
-
 public class OnReload {
-    public static void reLoadToMap() {
-        if (Main.PLUGIN.getConfig().getBoolean("convert_from_yaml", true)) {
-            File folder = new File(Main.PLUGIN.getDataFolder().getPath() + "/players/");
-            for (File file : folder.listFiles()) {
-                if (file.getName().toLowerCase().contains(".yml")) {
-                    new com.minegusta.mgskills.files.yaml.LoadToMap(UUID.fromString(file.getName().substring(0, file.getName().length() - 4)));
-                }
-            }
-            Main.PLUGIN.getConfig().set("convert_from_yaml", false);
-            Main.PLUGIN.saveConfig();
-        } else {
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                new LoadToMap(p);
-            }
+    public static void reLoadToMap()
+    {
+        for (Player p : Bukkit.getOnlinePlayers()) {
+            new LoadToMap(p);
         }
     }
 }
