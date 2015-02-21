d9221d7248298f9e3855a6b679e1abeacc8bba5f
Jan <janie177@hotmail.com>
2015-02-22 00:43:29 +0100

    Announcements will now show the right level. It was one too low.

--- src/main/java/com/minegusta/mgskills/util/LevelUpListener.java
+++ src/main/java/com/minegusta/mgskills/util/LevelUpListener.java
@@ -5,7 +5,7 @@ import org.bukkit.entity.Player;
 public class LevelUpListener {
     public static boolean isLevelUp(Player p, int exp, String skill, int level) {
         if (ExpTable.valueOf("L" + Integer.toString(level + 1)).getExp() <= exp && !(level == 100)) {
-            LevelUp.advanceLevel(p, skill, level);
+            LevelUp.advanceLevel(p, skill, level + 1);
             return isLevelUp(p, TempData.getMPlayer(p).getExp(Skill.valueOf(skill.toUpperCase())), skill, level + 1);
         }
         return false;
